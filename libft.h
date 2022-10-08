605272c89d02ff271bf11334f4893ac00898941e
Ayoub Aitouna <aaitouna@e1r1p4.1337.ma>
2022-10-08 21:53:44 +0100

    ft_split

--- libft.h
+++ libft.h
@@ -30,4 +30,5 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
 int		ft_atoi(const char *nptr);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 #endif
